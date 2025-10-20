ee66ca756ca452ca14d3f5bf114fc6debf1a98b0
ivan <ivan.v.dochev@gmail.com>
2025-10-20 18:26:20 +0800

    First draft of /api/register endpoint

---
+++ src/server/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using EnterpriseServer.Auth;
+using Microsoft.AspNetCore.Identity;
+using EnterpriseServer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnterpriseServer.Controllers;
+
+public class LoginController(
+    AppDbContext context,
+    UserManager<User> userManager
+    ) : BaseController(context)
+{
+
+    [HttpPost]
+    [Route("/api/register")]
+    public async Task<IActionResult> Register(User user) {
+        var result = await userManager.CreateAsync(user);
+
+        if (result.Succeeded)
+        {
+            var roleResult = await userManager.AddToRoleAsync(user, Roles.User);
+
+            if (!roleResult.Succeeded)
+            {
+                return BadRequest(roleResult.Errors);
+            }
+
+            return Ok();
+        }
+
+        return BadRequest(result.Errors);
+
+    }
+}
