8cebb9daec44789f8b2baf589876a07316d92c5f
ivan <ivan.v.dochev@gmail.com>
2025-10-20 18:25:40 +0800

    Removed unused using directive in AppDbContext.cs

--- src/server/AppDbContext.cs
+++ src/server/AppDbContext.cs
@@ -1,7 +1,6 @@
 
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using EnterpriseServer.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace EnterpriseServer;
