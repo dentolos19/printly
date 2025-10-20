7920bfcc6b4925e5cd9cb209683d12fbeb96a51f
ivan <ivan.v.dochev@gmail.com>
2025-10-20 16:11:59 +0800

    Updated the xml doc comment to reflect new AppDbContext name

--- src/server/Controllers/BaseController.cs
+++ src/server/Controllers/BaseController.cs
@@ -17,13 +17,13 @@ namespace EnterpriseServer.Controllers;
 /// <example>
 /// <code>
 /// //How to inherit from BaseController:
-/// public class MyController(Db context) : BaseController(context)
+/// public class MyController(AppDbContext context) : BaseController(context)
 /// {
 ///     //at this point, the db is already included
 /// }
 /// </code>
 /// </example>
-/// <param name="context">A reference to the database (this part is handled by ASP already via the builder instructions in the main Program.cs file), used inside each controller via the inherited property <see cref="Context"/></param>
+/// <param name="context">A readonly reference to the database (this part is handled by ASP already via the builder instructions in the main Program.cs file), used inside each controller via the inherited property <see cref="Context"/></param>
 [ApiController]
 public class BaseController(AppDbContext context) : ControllerBase
 {
