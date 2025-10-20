c39f221fcead1cf4ccd96d7b7425f41b0d70ada9
ivan <ivan.v.dochev@gmail.com>
2025-10-20 16:00:40 +0800

    Added initial claims

---
+++ src/server/Auth/Claims.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+
+namespace EnterpriseServer.Auth;
+
+/// <summary>
+/// A collection of C# <see cref="Claim"/> strings that
+/// </summary>
+public static class Claims
+{
+    /// <summary>Allows the user with this claim to edit/delete other users' data</summary>
+    public const string ManageUsers = "users.manage";
+
+    /// <summary>Allows the user to edit/delete their personal profile </summary>
+    public const string EditProfile = "profiles.edit";
+
+    /// <summary>Allows the user to change the role of other users</summary>
+    public const string ManageRoles = "roles.manage";
+
+    /// <summary>Allows the user to read data from the database</summary>
+    public const string ReadData = "data.read";
+}
