4d081c9bafc7bf015f8f45a877d6f731707a82a3
Dennise Catolos <dentolos19@gmail.com>
2025-10-30 14:40:53 +0800

    Added dashboard page

--- src/app/src/app/(platform)/dashboard/page.tsx
+++ src/app/src/app/(platform)/dashboard/page.tsx
@@ -1,5 +1,45 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { PlusIcon, ShirtIcon } from "lucide-react";
+
 export default function Page() {
-  return <div>Dashboard</div>;
+  return (
+    <div className={"p-6 w-full"}>
+      {/* Header */}
+      <div className={"flex justify-between mb-8"}>
+        <div>
+          <h1 className={"font-bold mb-2 text-4xl"}>Welcome to Makemix</h1>
+          <p className={"text-muted-foreground"}>This is your dashboard where you can manage your projects.</p>
+        </div>
+        <Button size={"lg"}>
+          <PlusIcon />
+          <span>New Design</span>
+        </Button>
+      </div>
+
+      {/* Templates */}
+      <div className={"flex gap-4 [&>*]:flex-1"}>
+        <Card>
+          <CardContent className={"flex flex-col items-center justify-center py-6"}>
+            <ShirtIcon className={"size-12"} />
+            <h2 className={"font-bold text-2xl mt-4"}>Make a Shirt</h2>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent className={"flex flex-col items-center justify-center py-6"}>
+            <ShirtIcon className={"size-12"} />
+            <h2 className={"font-bold text-2xl mt-4"}>Design a Mug</h2>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent className={"flex flex-col items-center justify-center py-6"}>
+            <ShirtIcon className={"size-12"} />
+            <h2 className={"font-bold text-2xl mt-4"}>Make Something Awesome</h2>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
---
+++ src/app/src/app/(platform)/layout.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import { useAuth } from "@/lib/providers/auth";
+import { BellIcon, LayoutDashboardIcon, LogOutIcon, MoreVerticalIcon, UserIcon } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+function NestedLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const { isMobile } = useSidebar();
+  const { logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+    router.push("/");
+  };
+
+  return (
+    <>
+      <Sidebar>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupLabel>Platform</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                <SidebarMenuItem>
+                  <SidebarMenuButton asChild>
+                    <Link href={"/dashboard"}>
+                      <LayoutDashboardIcon />
+                      <span>Dashboard</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuButton
+                    size="lg"
+                    className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                  >
+                    <Avatar className="h-8 w-8 rounded-lg grayscale">
+                      <AvatarImage src={""} alt={"John Doe"} />
+                      <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                    </Avatar>
+                    <div className="grid flex-1 text-left text-sm leading-tight">
+                      <span className="truncate font-medium">John Doe</span>
+                      <span className="text-muted-foreground truncate text-xs">john@example.com</span>
+                    </div>
+                    <MoreVerticalIcon className="ml-auto size-4" />
+                  </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+                  side={isMobile ? "bottom" : "right"}
+                  align="end"
+                  sideOffset={4}
+                >
+                  <DropdownMenuLabel className="p-0 font-normal">
+                    <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                      <Avatar className="h-8 w-8 rounded-lg">
+                        <AvatarImage src={""} alt={"User"} />
+                        <AvatarFallback className="rounded-lg">X</AvatarFallback>
+                      </Avatar>
+                      <div className="grid flex-1 text-left text-sm leading-tight">
+                        <span className="truncate font-medium">John Doe</span>
+                        <span className="text-muted-foreground truncate text-xs">john@example.com</span>
+                      </div>
+                    </div>
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                      <UserIcon />
+                      <span>Account</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <BellIcon />
+                      <span>Notifications</span>
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem onClick={handleLogout}>
+                    <LogOutIcon />
+                    <span>Logout</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+      </Sidebar>
+      {children}
+    </>
+  );
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <NestedLayout>{children}</NestedLayout>
+    </SidebarProvider>
+  );
+}
