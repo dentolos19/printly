6b0d7c8954d62f2537445c75bff91fb91b45bc78
Dennise Catolos <dentolos19@gmail.com>
2025-11-06 21:05:43 +0800

    Updated designer

--- src/app/src/app/(platform)/dashboard/page.tsx
+++ src/app/src/app/(platform)/dashboard/page.tsx
@@ -1,44 +1,23 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { PlusIcon, ShirtIcon } from "lucide-react";
+import { PlusIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <div className={"p-6 w-full"}>
-      {/* Header */}
       <div className={"flex justify-between mb-8"}>
         <div>
           <h1 className={"font-bold mb-2 text-4xl"}>Welcome to Mockly</h1>
           <p className={"text-muted-foreground"}>This is your dashboard where you can manage your projects.</p>
         </div>
-        <Button size={"lg"}>
-          <PlusIcon />
-          <span>New Design</span>
+        <Button size={"lg"} asChild>
+          <Link href={"/designer"}>
+            <PlusIcon />
+            <span>New Design</span>
+          </Link>
         </Button>
-      </div>
-
-      {/* Templates */}
-      <div className={"flex gap-4 [&>*]:flex-1"}>
-        <Card>
-          <CardContent className={"flex flex-col items-center justify-center py-6"}>
-            <ShirtIcon className={"size-12"} />
-            <h2 className={"font-bold text-2xl mt-4"}>Make a Shirt</h2>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardContent className={"flex flex-col items-center justify-center py-6"}>
-            <ShirtIcon className={"size-12"} />
-            <h2 className={"font-bold text-2xl mt-4"}>Design a Mug</h2>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardContent className={"flex flex-col items-center justify-center py-6"}>
-            <ShirtIcon className={"size-12"} />
-            <h2 className={"font-bold text-2xl mt-4"}>Make Something Awesome</h2>
-          </CardContent>
-        </Card>
       </div>
     </div>
   );
