ae879fd65453ec3d5687255e58da509533fcfe2a
Ivan <ivan.v.dochev@gmail.com>
2026-02-02 14:04:03 +0800

    Created user followers table to support following accounts on the platform

---
+++ src/server/Data/Entities/UserFollower.cs
@@ -0,0 +1,10 @@
+﻿namespace PrintlyServer.Data.Entities;
+
+public class UserFollower : BaseEntity
+{
+    public Guid FollowerId { get; set; }
+    public User Follower { get; set; }
+
+    public Guid FollowingId { get; set; }
+    public User Following { get; set; }
+}
