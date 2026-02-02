c6f9d7f8a108b6b3e3df6f3e174216f71b9d9f73
Ivan <ivan.v.dochev@gmail.com>
2026-02-02 14:22:19 +0800

    Adjusted User class to use the Roles.User const, and added the ICollections for the Followers and Following

--- src/server/Data/Entities/User.cs
+++ src/server/Data/Entities/User.cs
@@ -1,9 +1,13 @@
 using Microsoft.AspNetCore.Identity;
+using PrintlyServer.Data.Auth;
 
 namespace PrintlyServer.Data.Entities;
 
 public class User : IdentityUser
 {
     public List<RefreshToken> RefreshTokens { get; set; } = [];
-    public string Role { get; set; } = "User"; // "Admin" or "User"
+    public string Role { get; set; } = Roles.User; // Using the Roles class makes it safer
+
+    public ICollection<UserFollower> Followers { get; set; } = new List<UserFollower>();
+    public ICollection<UserFollower> Following { get; set; } = new List<UserFollower>();
 }
