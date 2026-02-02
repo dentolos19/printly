8b3d9d8bc34efbb50a6c5ccefd0d19eddf4345ef
Ivan <ivan.v.dochev@gmail.com>
2026-02-02 16:35:54 +0800

    Created Report and Block tables

---
+++ src/server/Data/Entities/UserBlock.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PrintlyServer.Data.Entities;
+
+public class UserBlock : BaseEntity
+{
+    [Required]
+    public string BlockerId { get; set; } = null!;
+
+    [ForeignKey(nameof(BlockerId))]
+    public User Blocker { get; set; } = null!;
+
+    [Required]
+    public string BlockedId { get; set; } = null!;
+
+    [ForeignKey(nameof(BlockedId))]
+    public User Blocked { get; set; } = null!;
+}
