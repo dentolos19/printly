9e05afb778399a7359251c32cd454d54a87b10bf
Dennise Catolos <dentolos19@gmail.com>
2026-02-14 20:08:07 +0800

    Updated worker configurations

--- src/server-worker/index.ts
+++ src/server-worker/index.ts
@@ -2,31 +2,14 @@ import { Container as CloudflareContainer, getContainer } from "@cloudflare/cont
 import { Hono } from "hono";
 
 export class Container extends CloudflareContainer<CloudflareEnv> {
-  // Port the container listens on (default: 8080)
   defaultPort = 8080;
-
-  // Time before container sleeps due to inactivity (default: 30s)
   sleepAfter = "10m";
-
-  // Environment variables passed to the container
   envVars = {
     ...(Object.fromEntries(Object.entries(process.env).filter(([, value]) => value !== undefined)) as Record<
       string,
       string
     >),
   };
-
-  override onStart() {
-    console.log("Container successfully started!");
-  }
-
-  override onStop() {
-    console.log("Container successfully stopped!");
-  }
-
-  override onError(error: any) {
-    console.error(error);
-  }
 }
 
 const app = new Hono<{ Bindings: CloudflareEnv }>();
